7db9eec10bbb39915bb046c3bd8af4db1ddcfc2f
iamrudyard <147968558+iamrudyard@users.noreply.github.com>
2026-02-16 16:11:57 +0800

    Update ParticipantsList.tsx

--- features/participants/ParticipantsList.tsx
+++ features/participants/ParticipantsList.tsx
@@ -367,7 +367,7 @@ const AttendanceList: React.FC = () => {
 
       <div className="flex flex-col xl:flex-row justify-between items-start xl:items-center gap-4 bg-slate-50 p-4 rounded-xl border border-slate-100">
         <div className="flex flex-col md:flex-row gap-4 w-full items-start md:items-center">
-            <div className="w-full md:w-[480px] relative" ref={dropdownRef}>
+            <div className="w-full md:w-[750px] relative" ref={dropdownRef}>
                 <div 
                     className="w-full bg-white border border-slate-300 rounded-lg px-4 py-2.5 flex justify-between items-center cursor-pointer hover:border-indigo-400 transition-colors shadow-sm"
                     onClick={() => setIsDropdownOpen(!isDropdownOpen)}
