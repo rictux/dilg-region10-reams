112c75ff8fdf38fe264f237b89cbf10fb4cf4aed
iamrudyard <147968558+iamrudyard@users.noreply.github.com>
2026-02-24 10:45:03 +0800

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js';
 // Safely access environment variables
 const env = (import.meta as any).env || {};
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
