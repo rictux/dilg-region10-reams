f4066fbdc41d385280ce9b1b09567d0a3846584c
iamrudyard <147968558+iamrudyard@users.noreply.github.com>
2026-03-06 11:46:03 +0800

    Update EventRegistration.tsx

--- features/events/EventRegistration.tsx
+++ features/events/EventRegistration.tsx
@@ -259,7 +259,7 @@ const EventRegistration: React.FC = () => {
                 f_name: toProperCase(formData.f_name.trim()),
                 l_name: toProperCase(formData.l_name.trim()),
                 m_initial: formData.m_initial.trim() === '' ? null : formData.m_initial.trim().toUpperCase(),
-                suffix: formData.suffix.trim() === '' ? null : toProperCase(formData.suffix.trim()),
+                suffix: formData.suffix.trim() === '' ? null : formData.suffix.trim(),
                 gender: formData.gender,
                 position: formData.position,
                 office: finalOfficeName,
